b06ac76d2513f49f05dbd8b50dce2f2354479b3f
DishantVaghela <38920453+DishantVaghela@users.noreply.github.com>
2018-05-10 09:59:40 +0530

    Update FileName.js

--- FileName.js
+++ FileName.js
@@ -2,7 +2,7 @@ function getHTMLFileName() {
 	fileName = location.href.split("/").slice(-1);
 	return fileName;
 }
-function removeExtentionFromHTMLFileName(fileName) {
+function removeExtentionFromFileName(fileName) {
 	fileNameWithoutExtention = /[^.]*/.exec(fileName)[0];
 	return fileNameWithoutExtention;
 }
