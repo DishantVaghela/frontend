72f6b3b54d4aa2b0aa4bc957c70b25ded5cc5eeb
DishantVaghela <38920453+DishantVaghela@users.noreply.github.com>
2018-05-10 15:20:18 +0530

    Update Title.js

--- Title.js
+++ Title.js
@@ -2,6 +2,5 @@ function setHTMLHeadTitle(headTitle) {
 	document.getElementsByTagName("title")[0].innerHTML = headTitle;
 }
 function setHTMLHeadTitleAutomatically() {
-	fileName = location.pathname.split("/").slice(-1);
-	setHTMLHeadTitle(/[^.]*/.exec(fileName));
+	setHTMLHeadTitle(/[^.]*/.exec(location.pathname.split("/").slice(-1)));
 }
