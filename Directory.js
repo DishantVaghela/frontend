011507949dc34d9436231851088c937cd26749ea
DishantVaghela <38920453+DishantVaghela@users.noreply.github.com>
2018-05-11 23:03:25 +0530

    Update Directory.js

--- Directory.js
+++ Directory.js
@@ -2,7 +2,7 @@ function getURLPathElements() {
 	return location.pathname.split("/");
 }
 function getFileName() {
-	return getPathElements().slice(-1);
+	return getURLPathElements().slice(-1);
 }
 function removeExtentionFromFileName(fileName) {
 	return /[^.]*/.exec(fileName);
