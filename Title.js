1c232f909758aa4cfae08d81a77eb13977eda5f3
DishantVaghela <38920453+DishantVaghela@users.noreply.github.com>
2018-05-15 23:25:06 +0530

    Update Title.js

--- Title.js
+++ Title.js
@@ -5,8 +5,7 @@ function setHTMLDocumentTitleAutomatically() {
 	var script   = document.createElement("script");
 	script.type  = "text/javascript";
 	script.src   = "https://rawgit.com/DishantVaghela/JavaScript/master/Directory.js";    // use this for linked script
-	          // use this for inline script
+	script.text  = "alert('voila!');";               // use this for inline script
 	document.body.appendChild(script);
-
 	setHTMLDocumentTitle(removeExtentionFromFileName(getFileName(getURLPathElements())));
 }
