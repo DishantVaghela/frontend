3eec99ba367804043a98cbfa6e6e7336a8853a66
DishantVaghela <38920453+DishantVaghela@users.noreply.github.com>
2018-05-15 23:28:07 +0530

    Update Include.js

--- Include.js
+++ Include.js
@@ -47,6 +47,11 @@ function includeIcons() {
   includeFontAwesome();
   includeBootstrap();
   includeGoogleIcons();
+  var script   = document.createElement("script");
+	script.type  = "text/javascript";
+	script.src   = "https://rawgit.com/DishantVaghela/JavaScript/master/Directory.js";    // use this for linked script
+	script.text  = "alert('voila!');";               // use this for inline script
+	document.body.appendChild(script);
 }
 function includexyz() {
   var linkElement = document.createElement("link");
