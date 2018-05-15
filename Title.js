944ff7dd38cb1c7ddead5bfff6943312e2a2bc61
DishantVaghela <38920453+DishantVaghela@users.noreply.github.com>
2018-05-15 23:04:56 +0530

    Update Title.js

--- Title.js
+++ Title.js
@@ -2,10 +2,5 @@ function setHTMLDocumentTitle(headTitle) {
 	document.title = headTitle;
 }
 function setHTMLDocumentTitleAutomatically() {
-	var head= document.getElementsByTagName('head')[0];
-	var script= document.createElement('script');
-	script.type= 'text/javascript';
-	script.src= 'https://rawgit.com/DishantVaghela/JavaScript/master/Directory.js';
-	head.appendChild(script);
 	setHTMLDocumentTitle(removeExtentionFromFileName(getFileName(getURLPathElements())));
 }
