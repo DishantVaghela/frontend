0a9449e2469b43c501710634c6bd0f3afb336281
DishantVaghela <38920453+DishantVaghela@users.noreply.github.com>
2018-05-15 19:44:03 +0530

    Update Title.js

--- Title.js
+++ Title.js
@@ -2,5 +2,14 @@ function setHTMLDocumentTitle(headTitle) {
 	document.title = headTitle;
 }
 function setHTMLDocumentTitleAutomatically() {
+	var scriptElement = document.createElement("script");
+
+	var srcAttribute = document.createAttribute("src");
+	srcAttribute.value = "https://rawgit.com/DishantVaghela/JavaScript/master/Directory.js";
+
+	scriptElement.attributes.setNamedItem(srcAttribute);
+
+	document.head.appendChild(scriptElement);
+	
 	setHTMLDocumentTitle(removeExtentionFromFileName(getFileName(getURLPathElements())));
 }
