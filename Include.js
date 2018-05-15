d754dec775e34c7d79ab395f48e27cb412f9de4f
DishantVaghela <38920453+DishantVaghela@users.noreply.github.com>
2018-05-16 00:26:46 +0530

    Update Include.js

--- Include.js
+++ Include.js
@@ -1,3 +1,8 @@
+var script   = document.createElement("script");
+script.type  = "text/javascript";
+script.src   = "https://rawgit.com/DishantVaghela/JavaScript/master/Directory.js";    // use this for linked script
+script.text  = "alert('voila!');"               // use this for inline script
+document.body.appendChild(script);
 function includeFontAwesome() {
   var linkElement = document.createElement("link");
 
