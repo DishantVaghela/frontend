81867631cb92a887e1db3467f1479669c1954256
DishantVaghela <38920453+DishantVaghela@users.noreply.github.com>
2018-05-16 16:37:45 +0530

    Update Include.js

--- Include.js
+++ Include.js
@@ -12,89 +12,23 @@ function includeScript(src) {
 	script.src   = src;    
 	document.body.appendChild(script);
 }
+function includeIcons() {
+  includeFontAwesome();
+  includeBootstrap();
+  includeGoogleIcons();
+}
 function includeFontAwesome() {
   	link("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css");
+}
+function includeBootstrap() {
+  	link("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css");
+}
+function includeGoogleIcons() {
+  	link("https://fonts.googleapis.com/icon?family=Material+Icons");
 }
 function link (href) {
 	var link = document.createElement("link");
 	link.rel = "stylesheet";
 	link.href = href;
 	document.head.appendChild(link);
-}
-function includeBootstrap() {
-  var linkElement = document.createElement("link");
-
-  var relAttribute = document.createAttribute("rel");
-  relAttribute.value = "stylesheet";
-
-  linkElement.attributes.setNamedItem(relAttribute);
-
-  var hrefAttribute = document.createAttribute("href");
-  hrefAttribute.value = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css";
-
-  linkElement.attributes.setNamedItem(hrefAttribute);
-
-  document.head.appendChild(linkElement);
-}
-function includeGoogleIcons() {
-  var linkElement = document.createElement("link");
-
-  var relAttribute = document.createAttribute("rel");
-  relAttribute.value = "stylesheet";
-
-  linkElement.attributes.setNamedItem(relAttribute);
-
-  var hrefAttribute = document.createAttribute("href");
-  hrefAttribute.value = "https://fonts.googleapis.com/icon?family=Material+Icons";
-
-  linkElement.attributes.setNamedItem(hrefAttribute);
-
-  document.head.appendChild(linkElement);
-}
-function includeIcons() {
-  includeFontAwesome();
-  includeBootstrap();
-  includeGoogleIcons();
-}
-function includexyz() {
-  var linkElement = document.createElement("link");
-
-  var relAttribute = document.createAttribute("rel");
-  relAttribute.value = "stylesheet";
-
-  linkElement.attributes.setNamedItem(relAttribute);
-
-  var hrefAttribute = document.createAttribute("href");
-  hrefAttribute.value = "https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css";
-
-  linkElement.attributes.setNamedItem(hrefAttribute);
-
-  document.head.appendChild(linkElement);
-
-  var scriptElement = document.createElement("script");
-
-  var srcAttribute = document.createAttribute("src");
-  srcAttribute.value = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
-
-  scriptElement.attributes.setNamedItem(srcAttribute);
-
-  document.head.appendChild(scriptElement);
-
-  var scriptElement1 = document.createElement("script");
-
-  var srcAttribute1 = document.createAttribute("src");
-  srcAttribute1.value = "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js";
-
-  scriptElement1.attributes.setNamedItem(srcAttribute1);
-
-  document.head.appendChild(scriptElement1);
-  
-  var scriptElement2 = document.createElement("script");
-
-  var srcAttribute2 = document.createAttribute("src");
-  srcAttribute2.value = "https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js";
-
-  scriptElement2.attributes.setNamedItem(srcAttribute2);
-
-  document.head.appendChild(scriptElement2);
 }
